bb71420f54e592960b3c06f8ce1df3039b3675f9
Mirko Ugolini <mirko.ugolini@gmail.com>
2021-10-14 17:15:14 +0200

    Add project files.

---
+++ Models/Actor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models{
+    public class Actor{
+        public int Id { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Surnanme { get; set; }
+
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+
+
+        public decimal WonOscars { get; set; }
+
+
+    }
+}
---
+++ Models/Director.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Director
+    {
+        public int Id { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Surnanme { get; set; }
+
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+
+        public decimal WonOscars { get; set; }
+
+    }
+}
+
